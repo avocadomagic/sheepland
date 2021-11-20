3f0cb50eba74b0a0a23e4a45f901d131271b3714
avocadomagic <94569436+avocadomagic@users.noreply.github.com>
2021-11-19 19:52:46 -0500

    Add files via upload

---
+++ wow.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    // alert("HIGHSCHOOL SWEETHEART<3");
+    $("#five").draggable();
+    $("#requirements").hover(function(){
+        $("#love").show();
+    });
+    $("#love").hover(function(){
+        $("#promise").show();
+    });
+    $("#promise").hover(function(){
+        $("#fuck").show();
+    });
+    $("#fuck").hover(function(){
+        $("#rip").show();
+    });
+})
+
+// command / cancels a line
